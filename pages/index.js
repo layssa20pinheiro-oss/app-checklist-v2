3cf78bdf23e1caf40ea3e4dfad374a199f4c609d
layssa20pinheiro-oss <layssa20pinheiro@gmail.com>
2026-04-16 16:25:54 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function HomeAdmin() {
         <img 
           src="/logo_nc_transparente.png" 
           alt="NC Cerimonial" 
-          className="h-20 mx-auto mb-8 object-contain" // Logo aumentada
+          className="h-20 mx-auto mb-6 object-contain" // Logo aumentada
         />
         <p className="text-[#ded0b8] text-[10px] uppercase font-bold tracking-[5px] mb-2">
           {saudacao}, {usuarioNome}
